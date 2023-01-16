1d6729d8b298a7d08756cab44df0cb759c53fbbf
christopher-k-c <chriscareyk@gmail.com>
2023-01-16 15:22:20 +0000

    Carousel implementation, experience component updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,86 @@
 import React from "react";
 import { NavBar } from "./components/NavBar";
 import { Landing } from "./components/Landing";
-import { Experience } from "./components/Experience.jsx";
-import { Projects } from "./components/Projects";
-
+import {ExperienceContainer} from './components/ExperienceContainer'
 
 
 function App() {
+  
+  const experience = [
+        
+    {   
+        'title': 'landing'
+    },    
+    {
+        'image': 'ga',
+        'title': 'Software Engineering Immersive Student', 
+        'company': 'General Assembly ', 
+        'dates': 'May 2022 - July 2022',
+        'skills': [
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-plain.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg"
 
+        ],
+        'summary': [
+
+            "Received 13 weeks of training in full-stack web development",
+            "Honed Fundamental skills in Javascript, CSS and HTML",
+            "Built Full and MERN Stack applications",
+            "Worked on group and individual projects using an agile methodology",
+            "Took part in daily coding warm-ups, lectures and live coding sessions",
+            "Developed good understanding of data structures and programming fundamentals",
+            "Developed a good understanding of MVC Architectural pattern",
+            "Implemented Full CRUD Operations in my Express and Django Applications",
+            "Built and used third party RESTful APIs",
+            "Used JWT Token to apply authentication and authorization to my RESTful APIs",
+            "Deployed our web applications using Heroku",
+            "Explored CSS Frameworks such as Tailwind and Bootstrap"
+            
+                
+        ],
+       
+    },
+    {
+        'image': 'jojo',
+        'title': 'Retouch Manger', 
+        'company': 'Jojo Maman Bebe', 
+        'dates': 'May 2015 - May 2022',
+        'skills': [
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/photoshop/photoshop-plain.svg",
+            require('./components/assets/captureone.png'),
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/filezilla/filezilla-plain.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/magento/magento-original.svg",
+
+        ],
+        'summary': [
+            "Managed a team of 2 retouchers and 1 photographer",
+            "Coordinated and booked freelance resources and external agencies",
+            "Managed the production process from beginning to end",
+            "Delegated to the team, ensured KPIs and deadlines were met",
+            "Interviewed, trained and compiled personal development reviews",
+            "Processing, editing, compositing and grading images using Adobe Photoshop and Capture One",
+            "Automated Adobe Photoshop processes",
+            "Working with Buying and Design on image amendments and colour correction",
+            "Built guidelines for internal and external use"
+            
+                
+        ],
+       
+    }
+]
 
 
 
@@ -23,12 +96,9 @@ function App() {
       </section>
 
       <section id="experience" className="snap-center">
-        <Experience/>
+        <ExperienceContainer experience={experience}></ExperienceContainer>
       </section>
 
-      <section>
-        <Projects/>  
-      </section>
     </div>
   );
 }
---
+++ src/components/ExperienceCards.jsx
@@ -0,0 +1,150 @@
+import React, {useState} from 'react'
+import {BsChevronCompactLeft, BsChevronCompactRight} from 'react-icons/bs'
+import {RxDotFilled} from 'react-icons/rx'
+import gif from './assets/terminal.gif'
+import {motion} from 'framer-motion'
+
+export const ExperienceCards = ({experience}) => {
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const prevSlide = () => {
+        const isFirstSlide = currentIndex === 0;
+        const newIndex = isFirstSlide ? experience.length - 1 : currentIndex - 1;
+        setCurrentIndex(newIndex)
+
+    }
+    const nextSlide = () => {
+        const isLastSlide = currentIndex === experience.length - 1;
+        const newIndex = isLastSlide ? 0 : currentIndex + 1;
+        setCurrentIndex(newIndex)
+    }
+    
+    const goToSlide = (slideIndex) => {
+        setCurrentIndex(slideIndex);
+    };
+
+  return (
+
+
+    // MAIN CONTAINER
+
+    
+    <div className='h-3/4 w-full py-20 px-10 relative group z-10'>
+
+        {/* IMAGE DIV CONTAINER */}
+        <div className='w-full h-full rounded-2xl bg-center bg-cover duration-500  bg-slate-800 bg-opacity-20 backdrop-blur-xl overflow-y-scroll no-scrollbar '>
+
+
+                {experience[currentIndex].title === 'landing' ? 
+                
+
+
+                    <>
+
+                        <div className='absolute flex flex-col p-10 '>
+                            <h3 className=' xl:text-7xl lg:text-6xl md:text-5xl sm:text-4xl text-3xl font-bold  text-slate-100 bg-gradient-to-r from-slate-400 via-indigo-600 to-indigo-600 text-transparent bg-clip-text'> Previously the Retouch Manager for Jojomamanbebe, now a General Assembly Software Engineer looking for my first role in the tech industry  </h3>
+                            <p className='w-full sm:w-1/2 pt-10 flex items-end xl:text-2xl text-1xl font-mono'>
+                                I am a recent General Assembly, Software Engineering Immersive Graduate with ten years of combined experience in e-commerce post-production and management. 
+                                My affinity for coding began with automation in Adobe Photoshop, sparking my ambition to pursue a career in development and, ultimately, my enrolment with General Assembly. 
+                            </p>
+                            <p className='w-1/2 pt-10 flex items-end xl:text-4xl md:text-2xl sm:text-2xl text-1xl font-mono'>                            
+                            </p>
+                        </div>
+                        <div className=' w-full h-full  '>
+                            <img className='object-cover w-full h-full mix-blend-luminosity opacity-10 blur' src={gif} alt="" />
+                        </div>
+
+                    </>
+                :
+                <>
+
+
+                <div className='grid grid-cols-1 place-items-center  '>
+
+
+                    <motion.div
+                    
+                    initial={{
+                        y:-200
+                    }}
+                    whileInView={{
+                        opacity: 1,
+                        y: 0
+                    }}
+                    transition={{
+                        duration: 1.2
+                    }}
+                    viewport={{
+                        one: true
+                    }}
+                    
+                    className='pt-10 pb-10' key={experience[currentIndex].title}>
+
+                        <img className='w-60 h-60 rounded-full bg-gray-900 object-contain object-center p-4 mt-4 flex items-center justify-center border-2 border-indigo-700 ' src={require('./assets/' + experience[currentIndex].image + '.png')} alt="" />
+                    </motion.div>
+
+                    <div className='pr-10 pl-10'>
+                        <h4 className='xl:text-6xl md:text-5xl sm:text-4xl text-3xl pb-2 font-bold mt-2 bg-gradient-to-r from-slate-400 via-indigo-600 to-indigo-600 text-transparent bg-clip-text'>{experience[currentIndex].title}</h4>
+                        
+                        <p className='xl:text-5xl md:text-4xl sm:text-3xl text-2xl pb-2 font-semibold mt-1 bg-gradient-to-r from-slate-400 via-indigo-600 to-indigo-600 text-transparent bg-clip-text'>{experience[currentIndex].company}</p>
+        
+                        <div className='grid grid-cols-5 pt-2 '>
+                            {experience[currentIndex].skills.map((skill) => (
+                                console.log(skill, "skill"),
+                                <img className='w-10 h-10 z-20 ' src={skill} />
+                            ))}
+                        </div>
+                            <p className='uppercase  text-slate-500 pt-4'>{experience[currentIndex].dates}</p>
+        
+                        <ul className='list-disc ml-5 space-y-4 text-lg  font-mono'>
+        
+                            {experience[currentIndex].summary.map((nestedSkills) => (
+                                <li className='pt-2'>{nestedSkills}</li>
+                            ))}
+        
+                        </ul>
+                    </div>
+
+                    </div>
+
+
+                    
+                    </>
+                }
+
+                
+
+
+                
+
+
+
+
+
+        </div>
+
+
+                        {/* LEFT BUTTON */}
+                        <div onClick={prevSlide} className='absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 md:text-7xl text-2xl rounded-full p-2 bg-black/50 text-white cursor-pointer z-20'>
+                <BsChevronCompactLeft  ></BsChevronCompactLeft>
+            </div>
+            {/* RIGHT BUTTON */}
+            <div onClick={nextSlide} className=' absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 md:text-7xl text-2xl rounded-full p-2 bg-black/50 text-white cursor-pointer z-20'>
+                <BsChevronCompactRight  ></BsChevronCompactRight>
+            </div>
+
+        {/* DOTS */}
+        
+        <div className='flex top-4 justify-center py-2'>
+            {experience.map((slides, slideIndex) => (
+                <div key={slideIndex} onClick={() => goToSlide(slideIndex)} className='text-2xl cursor-pointer '>
+                    <RxDotFilled></RxDotFilled>
+                </div>
+            ))}
+        </div>
+
+
+    </div>
+  )
+}
